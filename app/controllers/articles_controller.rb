701673fbf8ba163163de36f9640804424bf891b6
lucaskmiyazaki <lucaskmiyazaki@hotmail.com>
2020-06-23 16:37:17 -0300

    simple app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,9 @@ class ArticlesController < ApplicationController
  		@article.save
   		redirect_to @article
 	end
+	def index
+		@articles = Article.all
+	end
 	def show
 		@article = Article.find(params[:id])
 	end
